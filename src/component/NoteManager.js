0765229ec98964209123f953d63ade433e3211f3
olade014 <51375102+olade014@users.noreply.github.com>
2023-04-17 10:25:55 -0500

    Update NoteManager.js
    With start and build issues

--- src/component/NoteManager.js
+++ src/component/NoteManager.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from '../styles/NoteManager.module.css';
-import { FaPlus } from 'react-icons/fa';
+import styles from '../../styles/NoteManager.module.css';
 
 const NoteManager = ({ folders, folderManagerOpen }) => {
   const currentFolder = folders.find((folder) => folder.name === folderManagerOpen.title);
