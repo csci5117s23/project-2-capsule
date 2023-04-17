5089b446ec6a506249aef4f9262d32b23dc81020
olade014 <51375102+olade014@users.noreply.github.com>
2023-04-16 23:23:35 -0500

    Create NoteManager.js
    Put NoteManager in Component

---
+++ src/component/NoteManager.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from '../styles/NoteManager.module.css';
+import { FaPlus } from 'react-icons/fa';
+
+const NoteManager = ({ folders, folderManagerOpen }) => {
+  const currentFolder = folders.find((folder) => folder.name === folderManagerOpen.title);
+  const notes = currentFolder ? currentFolder.notes : [];
+
+  const handleClick = (note) => {
+    console.log(`Clicked on note: ${note.title}`);
+    // Add logic to navigate to note editor page and open note
+  }
+
+  return (
+    <div>
+      {currentFolder ? (
+        <div className={styles.noteList}>
+          {notes.map((note) => (
+            <div key={note.id} className={styles.note} onClick={() => handleClick(note)}>
+              <div className={styles.noteTitle}>{note.title || 'Untitled'}</div>
+              <div className={styles.noteContent}>{note.content}</div>
+              <div className={styles.noteActions}>
+                <div className={styles.noteMenu}>
+                  {note.title && (
+                    <div className={styles.noteMenuIcon}>
+                      <div className={styles.noteMenuDots}>...</div>
+                      <div className={styles.noteMenuContent}>
+                        <div>Download (Export)</div>
+                        <div>Copy</div>
+                        <div>Delete</div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className={styles.emptyFolder}>Folder is empty.</div>
+      )}
+    </div>
+  );
+};
+
+export default NoteManager;
