fd53bb301dc83d3a11f3c00db7e44db2cdc2e720
olade014 <51375102+olade014@users.noreply.github.com>
2023-04-16 23:37:46 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import 'react-quill/dist/quill.snow.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LeftHamburgerMenu from '../components/LeftHamburgerMenu/LeftHamburgerMenu';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
